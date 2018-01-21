01c9a99c8a0e3178a3823e04b54d184cd8cbd322
angelsl <angelsl@in04.sg>
2018-01-22 00:02:25 +0800

    Make a helper method for unbase64

--- java/src/test/java/io/github/angelsl/java/libkdbx/CryptoTest.java
+++ java/src/test/java/io/github/angelsl/java/libkdbx/CryptoTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Base64;
 
+import static io.github.angelsl.java.libkdbx.TestUtil.unb64;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class CryptoTest {
-    private static Base64.Decoder _b64d = Base64.getDecoder();
     @Test
     public void passwordTest() throws Throwable {
         assertArrayEquals(Crypto.deriveKeePassKey("aaaaa".getBytes("UTF-8"), null),
-                _b64d.decode("vCimDDOm83xb2mBLqgg6eK/Btw64uhnXeThcYHyFPsY="));
+                unb64("vCimDDOm83xb2mBLqgg6eK/Btw64uhnXeThcYHyFPsY="));
     }
 }
--- java/src/test/java/io/github/angelsl/java/libkdbx/TestUtil.java
+++ java/src/test/java/io/github/angelsl/java/libkdbx/TestUtil.java
@@ -3,8 +3,19 @@ package io.github.angelsl.java.libkdbx;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 
 public class TestUtil {
+    private static Base64.Decoder _b64d = Base64.getDecoder();
+
+    public static byte[] unb64(String b64) {
+        return _b64d.decode(b64);
+    }
+
+    public static byte[] unb64(byte[] b64) {
+        return _b64d.decode(b64);
+    }
+
     public static byte[] getResource(String path) {
         InputStream is = TestUtil.class.getClassLoader().getResourceAsStream(path);
         if (is == null) {
